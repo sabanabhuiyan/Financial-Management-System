f18489b58df40476c0e9d7ae55438f102f883f32
sabanabhuiyan <sabanabhuiyan10@gmail.com>
2024-09-03 10:49:38 -0400

    Add files via upload

--- STC/test/java/com/orange/testpages/PIMPageTest.java
+++ STC/test/java/com/orange/testpages/PIMPageTest.java
@@ -1,1 +1,59 @@
+package com.Orange.testPages;
 
+import com.Orange.BasesClass.BaseClass;
+import com.Orange.Pages.*;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class PIMPageTest extends BaseClass {
+    EmployeeListPage employeeListPage;
+    LoginPage loginPage;
+    HomePage homePage;
+    PIMPage pimPage;
+
+    PIMConfigurationPage PIMConfigurationPage;
+    public PIMPageTest(){
+        super();
+    }
+    @BeforeMethod
+    public void setup() {
+        initialization ();
+      employeeListPage = new EmployeeListPage ();
+        PIMConfigurationPage = new PIMConfigurationPage ();
+        loginPage = new LoginPage ();
+        homePage = new HomePage ();
+        pimPage = new  PIMPage ();
+        homePage = loginPage.validateLoginButton (prop.getProperty ("user"), prop.getProperty ("pass"));
+        pimPage =homePage.checkPIMLink ();
+
+    }
+    @Test(priority = 1)
+    public void verifyEmpInfoTextTest(){
+       Assert.assertTrue ( pimPage.validateEmpInfoText ());
+    }
+    @Test(priority = 2)
+    public void verifyEmployeeListLink(){
+      EmployeeListPage employeeListPage = pimPage.validateEmployeeListLink ();
+      Assert.assertNotNull (employeeListPage);
+    }
+    @Test(priority = 3)
+    public void verifyAddEmployeeLinkTest(){
+        PIMAddEmployeePage addEmployeePage = pimPage.validateAddEmployeeLink ();
+        Assert.assertNotNull (addEmployeePage);
+    }
+    @Test(priority = 4)
+     public void verifyConfigurationLinkTest(){
+       PIMConfigurationPage = pimPage.validateConfigurationLink ();
+       Assert.assertNotNull (PIMConfigurationPage);
+
+
+    }
+
+
+    @AfterMethod
+    public void TearDown(){
+        driver.quit ();
+    }
+}

645957f56d14cb067e17edc17cdb93c2f3315992
sabanabhuiyan <sabanabhuiyan10@gmail.com>
2024-09-03 10:42:25 -0400

    Add files via upload

--- STC/test/java/com/orange/testpages/ExcelsheetT.java
+++ STC/test/java/com/orange/testpages/ExcelsheetT.java
@@ -1,1 +1,49 @@
+package com.Orange.testPages;
 
+import com.Orange.ExcelUtility.XLxReader;
+
+public class ExcelsheetT {
+    public static void main(String[] args) {
+
+//    XLxReader reader = new XLxReader ("C:\\Users\\Lenovogs\\Desktop\\HRMOrange\\com.OrangeHRM\\src\\main\\java\\com\\Orange\\TestData\\EmployeesPicture.xlsx");
+//
+//        int rowCount = reader.getRowCount ("AddEmployee");
+//        for (int RowN = 2; RowN<=rowCount; RowN++){
+//            String firstN = reader.getCellData ("AddEmployee", "FirstName", RowN);
+//            System.out.println (firstN);
+//            String MidleN = reader.getCellData ("AddEmployee", "MidleName", RowN);
+//            System.out.println (MidleN);
+//            String LastN = reader.getCellData ("AddEmployee", "LastName", RowN);
+//            System.out.println (LastN);
+//            String AddPic = reader.getCellData ("AddEmployee", "AddPicture", RowN);
+//            System.out.println (AddPic);
+//
+//        }
+
+
+
+    }
+}
+//    XLxReader reader = new XLxReader ("C:\\Users\\Lenovogs\\Desktop\\HRMOrange\\com.OrangeHRM\\src\\main\\java\\com\\Orange\\TestData\\EmployeesPicture.xlsx");
+//    int rowCount = reader.getRowCount ("AddEmployee");
+//
+//        for (int RowN = 2; RowN <= rowCount; RowN++) {
+//
+//                String firstCol = reader.getCellData ("AddEmployee", "FirstName", RowN);
+//                System.out.println (firstCol);
+//                String MidleCol = reader.getCellData ("AddEmployee", "MidleName", RowN);
+//                System.out.println (MidleCol);
+//                String LastNCol = reader.getCellData ("AddEmployee", "LastName", RowN);
+//                System.out.println (LastNCol);
+//                String AddPicCol = reader.getCellData ("AddEmployee", "AddPicture", RowN);
+//                System.out.println (AddPicCol);
+//                firstN.sendKeys ("FirstName");
+//                MiddleN.sendKeys ("MidleName");
+//                lastN.sendKeys ("LastName");
+//                addPicture.sendKeys ("AddPicture");
+//                saveButton.click ();
+//                addEmplLink.click ();
+//                }
+//
+//                return new EmployeeListPage ();
+//                }
